bff25d941736d8bd3169ba6a26d82e5ff80774bf
unknown <aranha.nickolas@gmail.com>
2026-07-24 17:23:00 -0300

    chore: initialize project

---
+++ src/main/java/com/nickolasaranha/atm_api/Application.java
@@ -0,0 +1,15 @@
+package com.nickolasaranha.atm_api;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
+		builder.bannerMode(Banner.Mode.OFF);
+		builder.run(args);
+	}
+}
---
+++ src/test/java/com/nickolasaranha/atm_api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickolasaranha.atm_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
